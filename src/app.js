187f46a0de6440ceafa0cbba7725cffdf2ddf2e4
EScott237 <143569907+EScott237@users.noreply.github.com>
2023-10-23 22:31:11 +0000

    Update

--- src/app.js
+++ src/app.js
@@ -5,7 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronouns = ["the", "our"];
+let adjectives = ["great", "big"];
+let nouns = ["jogger", "racoon"];
+
+function generateDomainNames(pronouns, adjectives, nouns) {
+  let domainNames = [];
+
+  pronouns.forEach(pronoun => {
+    adjectives.forEach(adj => {
+      nouns.forEach(noun => {
+        const domainName = `${pronoun}${adj}${noun}.com`;
+        domainNames.push(domainName);
+      });
+    });
+  });
+
+  return domainNames;
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const domainNameCombinations = generateDomainNames(
+    pronouns,
+    adjectives,
+    nouns
+  );
+  console.log(domainNameCombinations);
 };
